9c797504157c9620c0194472bb83ec4658f1d34f
Raleigh-Foster <elementalmagickscardgame@yahoo.com>
2018-04-07 11:53:36 -0700

    work

---
+++ api/node/api/models/notification.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const notificationSchema = new Schema({
+    username: String,
+    searchQuery : String
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
