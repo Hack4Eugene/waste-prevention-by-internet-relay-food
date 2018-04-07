5aa2bbb0659ab506be2961df95e873d563b14457
Mel Stanley <mel.stanley@gmail.com>
2018-04-07 19:03:45 +0000

    Adding auth stuff

--- api/node/api/controllers/posts.js
+++ api/node/api/controllers/posts.js
@@ -5,6 +5,7 @@
 
 const util = require('util');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const Post = mongoose.models.Post;
 
 const AWS = require('aws-sdk');
@@ -19,6 +20,9 @@ const s3 = new AWS.S3({
   apiVersion: '2006-03-01',
   params: {Bucket: 'irf-pics'}
 });
+
+// Auth0 key for verifying auth tokens
+const authKey = "MIIC9TCCAd2gAwIBAgIJT/D4Z70A53iZMA0GCSqGSIb3DQEBCwUAMBgxFjAUBgNVBAMTDWlyZi5hdXRoMC5jb20wHhcNMTgwNDA3MDY0MDA4WhcNMzExMjE1MDY0MDA4WjAYMRYwFAYDVQQDEw1pcmYuYXV0aDAuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwnl3R1vlP7G63vk5vTwdG7XKIJRyOtw38jkVpZ754JMhr7cxIefb6cqrhmmVA2atB90P5sQILVdfq4Jo7y+dBBGL6ZtnPSUnWWvISMCYsJi0Wbbc4HlZZMlC3hLP2isZL70RLcBJWQbuAFM5XH8nutJTjqj1KQbjxMkn5892JQMuchtjr6iTnIu00bFy/7lWm6pIWAAKICFkvntXadEQhEt6CHA9QcRLuUy2bOjgHFY+CBqFVfzlJ/kfvNISeuf8Rp0h1v7kaB2r4wGAEx5DK28EKCp3ZDeqvrHEPeHc6UA/e0Y8Oi2dfdMyphvjwLl0lKIBi8MJmelAbqzOtensiQIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTDk/6ggHGnZgmQGjQpsojN2RzphDAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEBAKmB4lF5cFNFpn8tuZEVbILvzkGwxTXy2zz8IPStrCLa8YCyjQjcsKF3kss+Oay8WbXqjEIIsc0Kzox/Z0GfbUdgThv1ADzu8DQLDmoyyBTUAErbjo9yflVKqiwY7mT7KzN6CaT6e6h9wpWKKbjPSXjRZfxR1+NGENx3/wytA3zirWhv9/hxz3hxC7d6nu75MJGkWomoYS7d8uvOQR6Lt+QMJJqlc05qMRkCPflvq4ik1CYO6GTfHZp+TYfL5tOlZBo8GzVZVqK0Dtt710d5jftzn6j8iv2pSuy/ydxzhsD8bhDMDCIgMQEJ/5DbdKK7g/ZRUuqNBCS+JWaR9dBaPAI=";
 
 // Expose API endpoints
 module.exports = {
@@ -110,15 +114,33 @@ function getPost(req, res) {
 }
 
 function addPost(req, res) {
-
-    const post = Post(req.body);
-    post.save(function (error) {
-      if (error) {
-        res.status(500).send('Error saving new post: ' + util.inspect(error));
-      } else {
-        res.status(200).end(); // No body will be sent
-      }
-    });
+    var authHeader = req.header("Authorization");
+    //if (!authHeader) {
+    //  res.status(403).send('No authorization header received');
+    //  return;
+   // }
+    //var parts = authHeader.split(" ");
+    //if (parts.length < 2) {
+    //  res.status(403).send('Invalid authorization header');
+    //  return;
+    //}
+    //var token = parts[1];
+    //console.log("Auth Token: " + token);
+    //jwt.verify(token, authKey, { "algorithms": [ "HS256" ], "issuer": "https://irf.auth0.com/"}, function (err, decoded) {
+    //  if (err) {
+    //    res.status(403).send('Error processing token: ' + util.inspect(error));
+    //    return;
+    //  }
+      
+      const post = Post(req.body);
+      post.save(function (error) {
+        if (error) {
+          res.status(500).send('Error saving new post: ' + util.inspect(error));
+        } else {
+          res.status(200).end(); // No body will be sent
+        }
+      });
+    //});
 
 }
 
