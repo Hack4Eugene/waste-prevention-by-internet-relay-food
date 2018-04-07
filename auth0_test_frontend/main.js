8f102004bef5a3f712a62128897d64f700df33c8
Alex Anderson <alexandersonone@gmail.com>
2018-04-07 13:22:13 -0700

    Create Frontend test

---
+++ auth0_test_frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', function() {
+
+  var webAuth = new auth0.WebAuth({
+    domain: 'irf.auth0.com',
+    clientID: 'oMXIaq9Uw2TkRiF5GPvH2122c5IFF5g2',
+    responseType: 'token id_token',
+    audience: 'https://irf.auth0.com/api/v2/',
+    scope: 'openid email profile',
+    redirectUri: "http://localhost:10100/"
+  });
+
+  var loginBtn = document.getElementById('btn-login');
+
+  loginBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    webAuth.authorize();
+  });
+
+});
